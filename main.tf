89ffb5efb8f4e8c90bbaad1789857a20b31d759c
Nagababu <nagababu.rachakonda@gmail.com>
2026-03-03 23:32:46 +0530

    adding the file

--- main.tf
+++ main.tf
@@ -13,3 +13,14 @@ resource "aws_internet_gateway" "main" {
 
   tags = local.igw_final_tags
 }
+
+# subnet creating
+resource "aws_subnet" "public" {
+    count = length(var.public_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr[count.index]
+
+  tags = {
+    Name = "Main"
+  }
+}
