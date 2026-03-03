4fc9a1e8e279c580b3305bef59479f2dc326571f
Nagababu <nagababu.rachakonda@gmail.com>
2026-03-03 22:56:55 +0530

    adding the file

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+# aws vpc 
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
   tags = local.vpc_final_tags
+}
+
+# Internet gateway 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id  # Associating with VPC
+
+  tags = local.igw_final_tags
 }
