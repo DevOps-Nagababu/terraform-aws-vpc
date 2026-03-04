7c72ac7afb45a5ca0d67b5ff2290f65346c6512f
Nagababu <nagababu.rachakonda@gmail.com>
2026-03-04 13:01:19 +0530

    adding the file

--- main.tf
+++ main.tf
@@ -107,3 +107,54 @@ resource "aws_route_table" "database" {
     var.database_route_table_tags
   )
 }
+
+# aws Route public
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+# Elastic IP
+resource "aws_eip" "nat" {
+  domain                    = "vpc"
+   tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-nat"
+    },
+    var.eip_tags
+  )
+}
+# NAT gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    },
+    var.nat_gateway_tags
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+# aws Route private
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+# aws Route database
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
