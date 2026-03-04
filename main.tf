46eca9b70af0a3b22790c32a7f41f63125f5fd34
Nagababu <nagababu.rachakonda@gmail.com>
2026-03-04 12:25:53 +0530

    adding the file

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw_final_tags
 }
 
-# subnet creating
+# public subnet creating
 resource "aws_subnet" "public" {
-    count = length(var.public_subnet_cidr)
+  count = length(var.public_subnet_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidr[count.index]
   availability_zone = local.az_names[count.index]
@@ -29,5 +29,81 @@ resource "aws_subnet" "public" {
         Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
     },
     var.public_subnet_tags
+  )
+}
+
+# private subnet creating
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+  
+  tags = merge(
+    local.common_tags,
+    # roboshop-dev-public-us-east-1a
+    {
+        Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+    },
+    var.private_subnet_tags
+  )
+}
+
+# database subnet creating
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+ 
+  tags = merge(
+    local.common_tags,
+    # roboshop-dev-public-us-east-1a
+    {
+        Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+    },
+    var.database_subnet_tags
+  )
+}
+
+# Route table public
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    # roboshop-dev-public
+    {
+        Name = "${var.project}-${var.environment}-public"
+    },
+    var.public_route_table_tags
+  )
+}
+
+# Route table private
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    # roboshop-dev-private
+    {
+        Name = "${var.project}-${var.environment}-private"
+    },
+    var.private_route_table_tags
+  )
+}
+
+# Route table database
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    # roboshop-dev-database
+    {
+        Name = "${var.project}-${var.environment}-database"
+    },
+    var.database_route_table_tags
   )
 }
